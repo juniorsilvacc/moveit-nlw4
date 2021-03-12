013b0242b6684ae88353083df699c154942070a3
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-03-12 15:36:37 -0300

    function countdown p2

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,28 +1,47 @@
 import { useState, useEffect } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
+let countdownTimeout: NodeJS.Timeout;
+
 export function Countdown(){
 
-    const [time, setTime] = useState(25*60);
-    const [active, setActive] = useState(false);
+    const [time, setTime] = useState(1*6);
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
 
     const minutes = Math.floor(time/60);
-    const secunds = time % 60;
+    const seconds = time % 60;
 
+    // Transformar minutes e secunds em String
+    // padStart -> Se não tiver 2 caracter adicione um 0
+    // split -> reparti em duas strings por exemplo: '2' '5'
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
-    const [secundLeft, secundRight] = String(secunds).padStart(2, '0').split('');
+    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
+    // Quando clicar no button o setActive vai ser true
     function startCountDown(){
-        setActive(true)
+        setIsActive(true);
     }
 
+    function resetCountdown(){
+        clearTimeout(countdownTimeout);
+        setIsActive(false);
+        setTime(1*6);
+    }
+
+    // Quero executar isso toda vez que o active e time mudar
+    // Se countDown active tiver ativo e time for menor que 0
+    // Quero que o time reduza -1s a cada segundo contado pelo setTimeout
     useEffect(() => {
-        if(active && time > 0){
-            setTimeout(() => {
+        if(isActive && time > 0){
+            countdownTimeout = setTimeout(() => {
                 setTime(time - 1);
             }, 1000);
+        } else if (isActive && time == 0){
+            setHasFinished(true);
+            setIsActive(false);
         }
-    }, [active, time])
+    }, [isActive, time])
 
     return(
         <div>
@@ -33,18 +52,46 @@ export function Countdown(){
                 </div>
                 <span>:</span>
                 <div>
-                    <span>{secundLeft}</span>
-                    <span>{secundRight}</span>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
                 </div>
             </div>
 
-            <button 
-            type="button" 
-            className={styles.countdownButton}
-            onClick={startCountDown}
-            >
-                Iniciar um ciclo
-            </button>
+            {hasFinished ? (
+                <button 
+                disabled
+                type="button" 
+                className={styles.countdownButton}
+                onClick={resetCountdown}
+                >
+                Ciclo encerrado
+                </button>
+            ) : (
+                <>
+                    {isActive ? (
+                    <button 
+                        type="button" 
+                        className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                        // Quando clicar no button ativar a função resetCountdown
+                        onClick={resetCountdown}
+                        >
+                        Abandonar ciclo
+                    </button>
+                ) : (
+                    <button 
+                        type="button" 
+                        className={styles.countdownButton}
+                        // Quando clicar no button ativar a função startCountDown
+                        onClick={startCountDown}
+                        >
+                        Iniciar um ciclo
+                    </button>
+                )}
+                </>
+            )}
+
+            
+           
         </div>
     );
 }
