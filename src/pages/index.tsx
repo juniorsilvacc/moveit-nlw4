0692ca21a7268960575e4f0b86616d8acd01a961
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-03-11 23:39:51 -0300

    components countdown/completedchallenges

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Porfile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+
+import Head from 'next/head';
 
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Porfile/>
+          <CompletedChallenges/>
+          <Countdown/>
+
         </div>
         <div>
 
